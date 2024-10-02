0b7ac86d245311851f6cedcdda3a484f1a4ae9df
34-43 <121276581+34-43@users.noreply.github.com>
2024-10-02 16:52:20 +0900

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# SpartaPlanner
+들어가야 할 내용
+- API 명세서
+- ERD
+- SQL 
---
+++ src/main/java/com/sparta/spartaplanner/SpartaPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spartaplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaPlannerApplication.class, args);
+    }
+
+}
