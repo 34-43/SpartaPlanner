3e55347a78b9f449e6ad26ec61d93fb6ec3e9bda
34-43 <121276581+34-43@users.noreply.github.com>
2024-10-04 12:57:51 +0900

    fix: DB 테이블 확정

--- schedule.sql
+++ schedule.sql
@@ -3,12 +3,12 @@ CREATE TABLE IF NOT EXISTS USER
 (
     id                  bigint          NOT NULL AUTO_INCREMENT,
     username            varchar(10)     NOT NULL,
-    email               varchar(100)    NOT NULL,
-    created_datetime    datetime        NOT NULL,
-    last_datetime       datetime        NOT NULL,
+    password            varchar(10)     NOT NULL,
+    email               varchar(100)    NOT NULL UNIQUE,
+    created_at          datetime        NOT NULL,
+    updated_at          datetime        NOT NULL,
     PRIMARY KEY (id)
 );
-# DROP TABLE USER;
 
 # plan 테이블 생성
 CREATE TABLE IF NOT EXISTS PLAN
@@ -17,10 +17,12 @@ CREATE TABLE IF NOT EXISTS PLAN
     user_id             bigint          NOT NULL,
     title               varchar(100)    NOT NULL,
     content             varchar(500)    NOT NULL,
-    password            varchar(10)     NOT NULL,
-    created_datetime    datetime        NOT NULL,
-    last_datetime       datetime        NOT NULL,
+    created_at          datetime        NOT NULL,
+    updated_at          datetime        NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES USER(id)
+    FOREIGN KEY (user_id) REFERENCES USER(id) ON DELETE CASCADE
 );
+
+
+# DROP TABLE USER;
 # DROP TABLE PLAN;
